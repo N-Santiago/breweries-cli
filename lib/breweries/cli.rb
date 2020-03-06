4cf7e77ae1131c80e0c6c547a9320a03b4020a12
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-06 06:47:00 +0000

    Test functionality with simple methods

--- lib/breweries/cli.rb
+++ lib/breweries/cli.rb
@@ -1,26 +1,26 @@
 class Breweries::CLI
   
-  #start should greet user 
-  #pull data from API 
-  #create custom objects
-  #all inside start method 
-  
   def start 
     puts "Hello there!"
     puts "Looking for the best craft beers in town, please wait." 
+    display_info
   end 
   
   def display_info 
     puts "Please enter location:"
     input = gets.strip.downcase
+    
+    if input == "city"
+      puts "************You'll love the following spots!************"
+      puts "List of Breweries"
+      display_info
+    else 
+      quit 
+    end 
+  end  
+  
+  def quit 
+    puts "Goodbye. Drink responsibly and enjoy."
   end 
-  
-  #provide input instructions and/or examples
-  #user input / location
-  #answer user input. should provide breweries by location 
-  #else - ask to try again 
-  
-  #exit command
-  #if exit == "Goodbye, drink responsibly and enjoy."
   
 end
