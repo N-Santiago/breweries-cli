8a8bbc983cc546d635b1d76c01424b4bc7d31ed6
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-06 06:22:40 +0000

    Enter puts

--- lib/breweries/cli.rb
+++ lib/breweries/cli.rb
@@ -1,8 +1,26 @@
 class Breweries::CLI
   
+  #start should greet user 
+  #pull data from API 
+  #create custom objects
+  #all inside start method 
+  
   def start 
     puts "Hello there!"
-    binding.pry 
+    puts "Looking for the best craft beers in town, please wait." 
   end 
+  
+  def display_info 
+    puts "Please enter location:"
+    input = gets.strip.downcase
+  end 
+  
+  #provide input instructions and/or examples
+  #user input / location
+  #answer user input. should provide breweries by location 
+  #else - ask to try again 
+  
+  #exit command
+  #if exit == "Goodbye, drink responsibly and enjoy."
   
 end
