f3f58a71a53b062d7a6e1950d10a60c7d00f198b
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-06 05:32:58 +0000

    Solve sintax error

--- lib/breweries.rb
+++ lib/breweries.rb
@@ -2,10 +2,8 @@
 #load files needed to run this app 
 
 require "breweries/version"
-<<<<<<< HEAD
 require "breweries/cli"
-=======
->>>>>>> 8db63871664e2f887c3c89206a2018ee59d532dc
+
 
 module Breweries
   class Error < StandardError; end
