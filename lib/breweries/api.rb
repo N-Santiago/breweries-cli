e8170f54660be55a0ad2211ee42c479c1491835d
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-09 03:31:36 +0000

    hoppy code

--- lib/breweries/api.rb
+++ lib/breweries/api.rb
@@ -4,7 +4,7 @@ class Breweries::API
     @breweries_hash = HTTParty.get("https://api.openbrewerydb.org/breweries?by_city=")
     breweries_obj = {
       name: @breweries_hash[1],
-      city: @breweries_hash[4]
+      city: @breweries_hash[4] 
     }
     Breweries::HoppyCode.new(breweries_obj)
    end
--- lib/breweries/hoppycode.rb
+++ lib/breweries/hoppycode.rb
@@ -1,6 +1,5 @@
 class Breweries::HoppyCode
   
-<<<<<<< HEAD
   attr_accessor :name, :city 
   
   @@all = []
@@ -8,25 +7,15 @@ class Breweries::HoppyCode
   def initialize(hash)
     hash.each {|key, value| self.send(("#{key}="), value)}
     save
-=======
-  @@all = []
- 
-  def initialize(hash)
-    
-    binding.pry 
->>>>>>> 7ea98e6f7d6bc47fdb329af5b98e279691b18a3c
-  end
- 
+  end 
+  
   def self.all
     @@all
   end
-  
-<<<<<<< HEAD
+
   def save
     @@all << []
     binding.pry 
   end  
   
-=======
->>>>>>> 7ea98e6f7d6bc47fdb329af5b98e279691b18a3c
 end  
