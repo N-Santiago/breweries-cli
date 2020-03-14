5bed6777df4a5ec465b436ae5ef55cf45fd3b522
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-14 18:39:27 +0000

    brew

---
+++ lib/breweries/cli.rb
@@ -0,0 +1,23 @@
+class Breweries::CLI
+  
+  def start 
+    puts "Hello!"
+    puts "------"
+    puts "Please enter your location"
+    input = gets.strip.downcase
+    @data = Breweries::API.get_breweries(input)
+    @objects = Breweries::HoppyCode.all 
+    display_info
+  end 
+  
+  def display_info 
+    puts ""
+  else
+    quit
+  end  
+  
+  def quit 
+    puts "Goodbye. Drink responsibly and enjoy." 
+  end   
+  
+end
